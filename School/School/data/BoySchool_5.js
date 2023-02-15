671eff1f137f3837b1d25bab899622bf021cdea0
VickyAnandsbsc <125436676+VickyAnandsbsc@users.noreply.github.com>
2023-02-15 13:07:41 +0530

    Add files via upload

---
+++ School/School/data/BoySchool_5.js
@@ -0,0 +1,9 @@
+var json_BoySchool_5 = {
+"type": "FeatureCollection",
+"name": "BoySchool_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Status": "Govt. School", "Nm_School": "Goverment Boys Senior Secondary School Hamirpur", "No_Teacher": "40", "No_Student": "550", "No_Class_R": "40", "Play_G": "Yes", "Drinking_W": "Yes", "Toilet": "Yes", "Library": "Yes", "Conf_hall": "Yes" }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Status": null, "Nm_School": null, "No_Teacher": null, "No_Student": null, "No_Class_R": null, "Play_G": null, "Drinking_W": null, "Toilet": null, "Library": null, "Conf_hall": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 76.520968603859501, 31.6877026333554 ], [ 76.521168478584116, 31.687559865694936 ], [ 76.521458772826804, 31.68736950881447 ], [ 76.521782379523643, 31.687202946544176 ], [ 76.522263030646798, 31.686746090031022 ], [ 76.522529530279485, 31.686293992439801 ], [ 76.522205923582646, 31.686198813999567 ], [ 76.521972736404109, 31.68596562682103 ], [ 76.521177996428037, 31.686693741888803 ], [ 76.521054264455813, 31.686555733150499 ], [ 76.520816318355116, 31.686727054342953 ], [ 76.520687827460961, 31.686841268471255 ], [ 76.520492711658335, 31.687050661039791 ], [ 76.520692586382893, 31.687260053608327 ], [ 76.520949568171488, 31.687564624616869 ], [ 76.520878184341427, 31.6876360084471 ], [ 76.520968603859501, 31.6877026333554 ] ] ] ] } }
+]
+}
---
+++ School/School/data/Supermagnet_8.js
@@ -0,0 +1,8 @@
+var json_Supermagnet_8 = {
+"type": "FeatureCollection",
+"name": "Supermagnet_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Status": "Govt. School", "Nm_School": "Supermagnet Senior Secondary School", "No_Teacher": "11", "No_Student": "90", "No_Class_R": "15", "Play_G": "yes", "Drinking_W": "Yes", "Toilet": "Yes", "Library": "Yes", "Parking": "No", "Conf_hall": "No", "School_Van": "No" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 76.519830458726005, 31.691624246202252 ], [ 76.519563959093318, 31.69195023236 ], [ 76.519613927774458, 31.691985924275173 ], [ 76.519585374242297, 31.692023995651311 ], [ 76.519756695434864, 31.692154866006547 ], [ 76.519875668485156, 31.692095379481316 ], [ 76.520025574528461, 31.691928817211021 ], [ 76.520056507521531, 31.691893125295849 ], [ 76.519928016627148, 31.691816982543628 ], [ 76.51998274423039, 31.691740839791635 ], [ 76.519830458726005, 31.691624246202252 ] ] ] ] } }
+]
+}
